f10e23de66c276cbc995e958673a1bfef7a8d55f
Lin <1935284104@qq.com>
2025-10-03 23:57:51 +0800

    Create Game_Config.lua

---
+++ Config/Game_Config.lua
@@ -0,0 +1,26 @@
+return {
+    [4483381587] = {
+        Name = "一个字面上的底板。｜(a literal baseplate.)",
+        ScriptUrl = ""
+    },
+    [126884695634066] = {
+        Name = "种植花园｜(Grow a Garden)",
+        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Grow_a_Garden.lua"
+    },
+    [76455837887178] = {
+        Name = "挖掘它｜(Dig it)",
+        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Dig_it.lua"
+    },
+    [99078474560152] = {
+        Name = "M.E.G.无尽现实｜(M.E.G. Endless Reality)",
+        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/M.E.G._Endless_Reality.lua"
+    },
+    [109983668079237] = {
+        Name = "偷走一粒脑红｜(Steal a Brainrot)",
+        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Steal_a_Brainrot.lua"
+    },
+    [96342491571673] = {
+        Name = "偷走一粒脑红｜(Steal a Brainrot)"，
+        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Steal_a_Brainrot.lua"
+    }
+}
