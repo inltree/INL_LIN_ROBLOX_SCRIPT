00fda1a0fd12ea695892f284458c288a901b3fec
Lin <1935284104@qq.com>
2025-10-03 19:32:29 +0800

    Update INL_SCRIPT_LOADER.lua

--- INL_SCRIPT_LOADER.lua
+++ INL_SCRIPT_LOADER.lua
@@ -43,6 +43,10 @@ local GAME_CONFIG = {
         Name = "M.E.G.无尽现实｜(M.E.G. Endless Reality)",
         ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/M.E.G._Endless_Reality.lua"
     },
+    [109983668079237] = {
+        Name = "偷走一粒脑红｜(Steal a Brainrot)",
+        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Steal_a_Brainrot.lua"
+    }
     [96342491571673] = {
         Name = "偷走一粒脑红｜(Steal a Brainrot)",
         ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Steal_a_Brainrot.lua"
