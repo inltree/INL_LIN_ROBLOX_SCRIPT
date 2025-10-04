a86d249967ae07ba7fddd79e5d5cad32a7f9fde8
Lin <1935284104@qq.com>
2025-10-04 16:48:39 +0800

    Update Game_Config.lua

--- Config/Game_Config.lua
+++ Config/Game_Config.lua
@@ -1,28 +1,10 @@
-local Config = {
-    [4483381587] = {
-        Name = "一个字面上的底板。｜(a literal baseplate.)",
-        ScriptUrl = ""
-    },
-    [126884695634066] = {
-        Name = "种植花园｜(Grow a Garden)",
-        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Grow_a_Garden.lua"
-    },
-    [76455837887178] = {
-        Name = "挖掘它｜(Dig it)",
-        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Dig_it.lua"
-    },
-    [99078474560152] = {
-        Name = "M.E.G.无尽现实｜(M.E.G. Endless Reality)",
-        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/M.E.G._Endless_Reality.lua"
-    },
-    [109983668079237] = {
-        Name = "偷走一粒脑红｜(Steal a Brainrot)",
-        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Steal_a_Brainrot.lua"
-    },
-    [96342491571673] = {
-        Name = "偷走一粒脑红｜(Steal a Brainrot)"，
-        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Steal_a_Brainrot.lua"
-    }
+local Games = {
+    [4483381587] = "" -- 一个字面上的底板。｜(a literal baseplate.)
+    [126884695634066] = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Grow_a_Garden.lua" -- 种植花园｜(Grow a Garden)
+    [76455837887178] = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Dig_it.lua" -- 挖掘它｜(Dig it)
+    [99078474560152] = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/M.E.G._Endless_Reality.lua" -- M.E.G.无尽现实｜(M.E.G. Endless Reality)
+    [109983668079237] = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Steal_a_Brainrot.lua" -- 偷走一粒脑红｜(Steal a Brainrot)
+    [96342491571673] = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Steal_a_Brainrot.lua" -- 偷走一粒脑红｜(Steal a Brainrot)
 }
 
-return Config
+return Games
