5088563f6e68d18f8ffbe6b73f8d1429b589f28b
Lin <1935284104@qq.com>
2025-10-16 22:45:12 +0800

    Refactor game configuration structure

--- Config/Game_Config.lua
+++ Config/Game_Config.lua
@@ -1,10 +1,28 @@
-local Games = {
-    [4483381587] = "",  -- 空配置（无脚本）
-    [126884695634066] = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Grow_a_Garden.lua",
-    [76455837887178] = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Dig_it.lua",
-    [99078474560152] = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/M.E.G._Endless_Reality/Mua",
-    [109983668079237] = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Steal_a_Brainrot.lua",
-    [96342491571673] = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Steal_a_Brainrot.lua",
+local Config = {
+    [4483381587] = {
+        Name = "一个字面上的底板。｜(a literal baseplate.)",
+        ScriptUrl = ""
+    },
+    [126884695634066] = {
+        Name = "种植花园｜(Grow a Garden)",
+        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Grow_a_Garden.lua"
+    },
+    [76455837887178] = {
+        Name = "挖掘它｜(Dig it)",
+        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Dig_it.lua"
+    },
+    [99078474560152] = {
+        Name = "M.E.G.无尽现实｜(M.E.G. Endless Reality)",
+        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/M.E.G._Endless_Reality/lua"
+    },
+    [109983668079237] = {
+        Name = "偷走一粒脑红｜(Steal a Brainrot)",
+        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Steal_a_Brainrot.lua"
+    },
+    [96342491571673] = {
+        Name = "偷走一粒脑红｜(Steal a Brainrot)",
+        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Steal_a_Brainrot.lua"
+    }
 }
 
-return Games
+return Config
