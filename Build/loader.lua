6f2f7b40dcb85e29d7f7ba108fc8543946e74e2e
Lin <1935284104@qq.com>
2025-10-16 22:50:57 +0800

    Add SCRIPT_LOADER.luau file

--- Build/loader.lua
+++
@@ -1,9 +0,0 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Config/Game_Config.lua"))()
-
-local URL = Games[game.PlaceId]
-
-if URL and URL ~= "" then
-  loadstring(game:HttpGet(URL))()
-else
-  warn("No script configured for this game.")
-end
