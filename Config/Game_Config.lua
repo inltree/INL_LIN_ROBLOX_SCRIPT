393cde08b8053b8b3afcdb166070b2c3587951f5
Lin <1935284104@qq.com>
2025-10-16 18:32:51 +0800

    Fix syntax error in Game_Config.lua

--- Config/Game_Config.lua
+++ Config/Game_Config.lua
@@ -1,5 +1,5 @@
 local Games = {
-    [4483381587] = ""， -- 一个字面上的底板。｜(a literal baseplate.)
+    [4483381587] = " "， -- 一个字面上的底板。｜(a literal baseplate.)
     [126884695634066] = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Grow_a_Garden.lua"， -- 种植花园｜(Grow a Garden)
     [76455837887178] = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Dig_it.lua"， -- 挖掘它｜(Dig it)
     [99078474560152] = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/M.E.G._Endless_Reality.lua"， -- M.E.G.无尽现实｜(M.E.G. Endless Reality)
