4452a31aa5eaced91d5e72c4c83881c84c2547b6
Lin <1935284104@qq.com>
2025-10-16 18:40:22 +0800

    Update game configuration URLs and comments

--- Config/Game_Config.lua
+++ Config/Game_Config.lua
@@ -1,10 +1,10 @@
 local Games = {
-    [4483381587] = " "， -- 一个字面上的底板。｜(a literal baseplate.)
-    [126884695634066] = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Grow_a_Garden.lua"， -- 种植花园｜(Grow a Garden)
-    [76455837887178] = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Dig_it.lua"， -- 挖掘它｜(Dig it)
-    [99078474560152] = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/M.E.G._Endless_Reality.lua"， -- M.E.G.无尽现实｜(M.E.G. Endless Reality)
-    [109983668079237] = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Steal_a_Brainrot.lua"， -- 偷走一粒脑红｜(Steal a Brainrot)
-    [96342491571673] = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Steal_a_Brainrot.lua" -- 偷走一粒脑红｜(Steal a Brainrot)
+    [4483381587] = "",  -- 空配置（无脚本）
+    [126884695634066] = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Grow_a_Garden.lua",
+    [76455837887178] = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Dig_it.lua",
+    [99078474560152] = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/M.E.G._Endless_Reality/Mua",
+    [109983668079237] = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Steal_a_Brainrot.lua",
+    [96342491571673] = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Steal_a_Brainrot.lua",
 }
 
 return Games
