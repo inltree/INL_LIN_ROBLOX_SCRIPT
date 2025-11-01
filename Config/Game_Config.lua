3de7bbdb8e1ba3c946acdcd1fac4a9343273f061
Lin <1935284104@qq.com>
2025-11-01 12:26:29 +0800

    Update Game_Config.lua

--- Config/Game_Config.lua
+++ Config/Game_Config.lua
@@ -13,7 +13,7 @@ local Config = {
     },
     [99078474560152] = {
         Name = "M.E.G.无尽现实｜(M.E.G. Endless Reality)",
-        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/M.E.G._Endless_Reality/lua"
+        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/M.E.G._Endless_Reality.lua"
     },
     [109983668079237] = {
         Name = "偷走一粒脑红｜(Steal a Brainrot)",
